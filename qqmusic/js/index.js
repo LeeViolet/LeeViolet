62023c8166177fc2efdcff5b1a950a573a6a8e8c
lee <372298644@qq.com>
2021-08-10 20:03:32 +0800

    interview project

--- qqmusic/js/index.js
+++ qqmusic/js/index.js
@@ -115,25 +115,5 @@ $(function () {
         createPoint();
     });
 
-    // 模态框
-    $('.slide_list a').click(function () {
-        $('.mask').show();
-        $('.login_modal').show();
-        $('body').css({
-            overflow: 'hidden',
-            paddingRight: '10px',
-        });
-    });
-    $('.btn_close').click(function () {
-        $('.mask').hide();
-        $('.login_modal').hide();
-        $('body').css({
-            overflow: '',
-            paddingRight: '',
-        });
-    });
-    $('.login_hd a').click(function () {
-        $(this).addClass('current').siblings().removeClass('current');
-        $('.login_bd>div').eq($(this).index()).addClass('current').siblings().removeClass('current');
-    });
+
 });
